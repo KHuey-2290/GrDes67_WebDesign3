4bf7fee14142f05a17312ec37b4bbe077d6ca9d2
KHuey-2290 <huey_karissa_yayoi@student.smc.edu>
2020-11-17 16:35:34 -0800

    C2:(class) finished classwork for HTML canvas and interactive animation

--- Assignment10_HTMLCanvas_InteractiveAnimation/Classwork/canvas.js
+++ Assignment10_HTMLCanvas_InteractiveAnimation/Classwork/canvas.js
@@ -1,13 +1,23 @@
-const canvas = document.getElementById("Game");
+const canvas = document.getElementById("myCanvas");
 const context = canvas.getContext("2d");
 
-let cPosX = 400;
-let cPosY = 300;
+let cPosX = canvas.width/2;
+let cPosY = canvas.height/2;
 let cVelX = 3.4;
 let cVelY = 4.2;
 let cRad = 60;
 
 function draw () {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawCircle();
+
+    moveCircle();
+
+    window.requestAnimationFrame(draw);
+}
+
+function drawCircle() {
     context.beginPath();
     context.strokeStyle = "black";
     context.lineWidth = 2;
@@ -15,11 +25,34 @@ function draw () {
     context.ellipse(cPosX, cPosY, cRad, cRad, 0, 0, 2*Math.PI);
     context.stroke();
     context.fill();
+}
+
+function moveCircle() {
+    if(cPosX+cRad >= 800 || cPosX-cRad <= 0){
+        cVelX *= -1;
+    }
+
+    if(cPosY+cRad >= 600 || cPosY-cRad <= 0) {
+        cVelY = cVelY * -1;
+    }
 
     cPosX = cPosX + cVelX;
     cPosY = cPosY + cVelY;
-
-    window.requestAnimationFrame(draw);
 }
 
 draw();
+
+canvas.addEventListener("click", function(event){
+    console.log(event);
+    let mouseXp = event.pageX - event.target.offsetLeft;
+    let mouseYp = event.pageY - event.target.offsetTop;
+    console.log("Mouse X: " + mouseXp);
+    console.log("Mouse Y: " + mouseYp);
+
+    let distX = Math.abs(cPosX - mouseXp);
+    let distY = Math.abs(cPosY - mouseYp);
+
+    if(distX < cRad && distY < cRad) {
+        console.log("FIRE!!!!!");
+    }
+});
