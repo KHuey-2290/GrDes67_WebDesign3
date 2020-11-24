b5b89ba767dc1dc64502298002f42f64db8d25fc
KHuey-2290 <huey_karissa_yayoi@student.smc.edu>
2020-11-24 14:52:27 -0800

    C5: (assignemnt) figured out how to change colors of balal and put in a ball count

--- Assignment10_HTMLCanvas_InteractiveAnimation/Assignment/script.js
+++ Assignment10_HTMLCanvas_InteractiveAnimation/Assignment/script.js
@@ -1,11 +1,19 @@
 const canvas = document.getElementById("myCanvas");
 const context = canvas.getContext("2d");
 
+var clicks = 1;
+
 let cPosX = canvas.width/2;
 let cPosY = canvas.height/2;
 let cVelX = 3.4;
 let cVelY = 4.2;
 let cRad = 50;
+
+let cColor = "rgb(255, 200, 0)";
+
+let rSlider = document.getElementById("redSlider");
+let gSlider = document.getElementById("greenSlider");
+let bSlider = document.getElementById("blueSlider");
 
 function draw () {
     context.clearRect(0, 0, canvas.width, canvas.height);
@@ -22,7 +30,8 @@ function drawCircle() {
     context.boxSizing = "border-box";
     context.strokeStyle = "navy";
     context.lineWidth = 5;
-    context.fillStyle = "goldenrod";
+    // context.fillStyle = "goldenrod";
+    context.fillStyle = cColor;
     context.ellipse(cPosX, cPosY, cRad, cRad, 0, 0, 2*Math.PI);
     context.stroke();
     context.fill();
@@ -43,6 +52,11 @@ function moveCircle() {
 
 draw();
 
+let colorChange = function (){
+    console.log("Red: " + rSlider.value + "Green: " + gSlider.value + "Blue: " + bSlider.value);
+    cColor = "rgb("+ rSlider.value +", "+gSlider.value +", "+bSlider.value +")";
+}
+
 canvas.addEventListener("click", function(event){
     console.log(event);
 
@@ -57,8 +71,14 @@ canvas.addEventListener("click", function(event){
 
     if(distX < cRad && distY < cRad) {
         console.log("GOT EM!!!!!");
+        console.log(clicks);
+        clicks += 1;
     } 
     else {
         console.log("MISSED!! Try again");
     }
 });
+
+rSlider.addEventListener("change", colorChange);
+gSlider.addEventListener("change", colorChange);
+bSlider.addEventListener("change", colorChange);
