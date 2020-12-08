3d0194fec8e11dba2d0b1d922b851c4c0abcc497
KHuey-2290 <huey_karissa_yayoi@student.smc.edu>
2020-12-07 22:56:55 -0800

    C5: added images for carousel; added jumbotron html and css; added subscription modal popup BUT NOT FINISHED; cannot figure out validation.

---
+++ Assignment14_Final/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+    //Modal  
+    $("#myModal").modal("show");
+
+    $(".yes-btn").click(function(){
+        $("input:text").attr("disabled",!this.checked);
+    });
+
+    $("form.needs-validation").submit(function(event){
+        event.preventDefault();
+  
+        if($("input:text").val() === "" ){
+          $("form.needs-validation").removeClass('needs-validation').addClass('was-validated');
+        }
+      })
+
+    // Carousel
+    $(".carousel").carousel({
+        interval : 4000,
+        keyboard : false
+    });
+});
+
+
