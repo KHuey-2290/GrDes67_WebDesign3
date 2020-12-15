2b2501d83b94dcf76d8df0dc11400ea1cc7dc38b
KHuey-2290 <huey_karissa_yayoi@student.smc.edu>
2020-12-14 23:23:11 -0800

    C13: added carousel to each featured product page; added images; changed files names accordingly

--- Assignment14_Final/script.js
+++ Assignment14_Final/script.js
@@ -2,26 +2,153 @@
 
 $(document).ready(function() {
 
-    //Modal  
-    $("#myModal").modal("show");
+  //Modal  
+  $("#myModal").modal("show");
 
-    $(".yes-btn").click(function(){
-        $("input:text").attr("disabled",!this.checked);
-    });
+  $(".yes-btn").click(function(){
+      $("input:text").attr("disabled",!this.checked);
+  });
 
-    $("form.needs-validation").submit(function(event){
-        event.preventDefault();
+  $("form.needs-validation").submit(function(event){
+      event.preventDefault();
+
+      if($("input:text").val() === "" ){
+        $("form.needs-validation").removeClass('needs-validation').addClass('was-validated');
+      }
+    })
+
+  // Carousel
+  $(".carousel").carousel({
+      interval : 2000,
+      keyboard : false
+  });
   
-        if($("input:text").val() === "" ){
-          $("form.needs-validation").removeClass('needs-validation').addClass('was-validated');
-        }
-      })
 
-    // Carousel
-    $(".carousel").carousel({
-        interval : 4000,
-        keyboard : false
-    });
+  // Shop Page
+  $("#btnCart1").click(function(){
+    $("#btnCart1").text("Review Shopping Cart");
+    $("#btnCart1").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart1").css({color: '#091244'});
+  });
+
+  $("#btnCart2").click(function(){
+    $("#btnCart2").text("Review Shopping Cart");
+    $("#btnCart2").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart2").css({color: '#091244'});
+  });
+
+  $("#btnCart3").click(function(){
+    $("#btnCart3").text("Review Shopping Cart");
+    $("#btnCart3").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart3").css({color: '#091244'});
+  });
+
+  $("#btnCart4").click(function(){
+    $("#btnCart4").text("Review Shopping Cart");
+    $("#btnCart4").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart4").css({color: '#091244'});
+  });
+
+  $("#btnCart5").click(function(){
+    $("#btnCart5").text("Review Shopping Cart");
+    $("#btnCart5").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart5").css({color: '#091244'});
+  });
+
+  $("#btnCart6").click(function(){
+    $("#btnCart6").text("Review Shopping Cart");
+    $("#btnCart6").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart6").css({color: '#091244'});
+  });
+
+  $("#btnCart7").click(function(){
+    $("#btnCart7").text("Review Shopping Cart");
+    $("#btnCart7").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart7").css({color: '#091244'});
+  });
+
+  $("#btnCart8").click(function(){
+    $("#btnCart8").text("Review Shopping Cart");
+    $("#btnCart8").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart8").css({color: '#091244'});
+  });
+
+  $("#btnCart9").click(function(){
+    $("#btnCart9").text("Review Shopping Cart");
+    $("#btnCart9").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart9").css({color: '#091244'});
+  });
+
+  $("#btnCart10").click(function(){
+    $("#btnCart10").text("Review Shopping Cart");
+    $("#btnCart10").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart10").css({color: '#091244'});
+  });
+
+  $("#btnCart11").click(function(){
+    $("#btnCart11").text("Review Shopping Cart");
+    $("#btnCart11").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart11").css({color: '#091244'});
+  });
+
+  $("#btnCart12").click(function(){
+    $("#btnCart12").text("Review Shopping Cart");
+    $("#btnCart12").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart12").css({color: '#091244'});
+  });
+
+  $("#btnCart13").click(function(){
+    $("#btnCart13").text("Review Shopping Cart");
+    $("#btnCart13").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart13").css({color: '#091244'});
+  });
+
+  $("#btnCart14").click(function(){
+    $("#btnCart14").text("Review Shopping Cart");
+    $("#btnCart14").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart14").css({color: '#091244'});
+  });
+  
+  $("#btnCart15").click(function(){
+    $("#btnCart15").text("Review Shopping Cart");
+    $("#btnCart15").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart15").css({color: '#091244'});
+  });
+
+  $("#btnCart16").click(function(){
+    $("#btnCart16").text("Review Shopping Cart");
+    $("#btnCart16").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart16").css({color: '#091244'});
+  });
+
+  $("#btnCart17").click(function(){
+    $("#btnCart17").text("Review Shopping Cart");
+    $("#btnCart17").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart17").css({color: '#091244'});
+  });
+
+  $("#btnCart18").click(function(){
+    $("#btnCart18").text("Review Shopping Cart");
+    $("#btnCart18").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart18").css({color: '#091244'});
+  });
+
+  $("#btnCart19").click(function(){
+    $("#btnCart19").text("Review Shopping Cart");
+    $("#btnCart19").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart19").css({color: '#091244'});
+  });
+
+  $("#btnCart20").click(function(){
+    $("#btnCart20").text("Review Shopping Cart");
+    $("#btnCart20").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart20").css({color: '#091244'});
+  });
+
+  $("#btnCart21").click(function(){
+    $("#btnCart21").text("Review Shopping Cart");
+    $("#btnCart21").css({backgroundColor: '#9c9c9c'});
+    $("#btnCart21").css({color: '#091244'});
+  });
 });
-
 
